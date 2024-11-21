328ed0336255ad80aae693cc7db3253d49e8f24b
Jacob <jacobmalach@berkeley.edu>
2024-11-21 14:20:17 -0800

    added perms for frontend

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://your-backend-url.onrender.com'], 
+  origin: ['http://localhost:3000', 'https://your-backend-url.onrender.com', 'https://nineball.vercel.app/'], 
 }));
 app.use(express.json({limit: '25mb'}));
 
