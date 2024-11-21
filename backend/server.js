b61d94aaafa63710962424182c50c46674866d77
Jacob <jacobmalach@berkeley.edu>
2024-11-21 14:52:44 -0800

    fixed url

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https:/nineball.onrender.com'], 
+  origin: ['http://localhost:3000', 'https://nineball.onrender.com'], 
 }));
 app.use(express.json({limit: '25mb'}));
 
